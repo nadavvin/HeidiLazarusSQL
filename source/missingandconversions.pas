0ba7deec2dbd2b1892879bebf78033d1d0936313
Nadav Vinik <nadavvin@gmail.com>
2018-11-15 21:04:58 +0200

    main compile to line 5673

--- source/missingandconversions.pas
+++ source/missingandconversions.pas
@@ -8,15 +8,52 @@ uses
     Classes, SysUtils, SynCompletion;
 
 type
+    TSynCompletionProposalInsertList = class
+        procedure Add(str: String);
+    end;
+
+    TCustomSynEdit = class
+        procedure GetHighlighterAttriAtRowColEx();
+    end;
+
+    TSynCompletionProposalForm = class
+        function CurrentEditor() : TCustomSynEdit;
+    end;
+
     SynCompletionType = class
 
     end;
 
   TSynCompletionProposal = class(TSynCompletion)
-
+    var
+       InsertList: TSynCompletionProposalInsertList;
+       Form: TSynCompletionProposalForm;
+    procedure AddItem(str: String; str2: String);
+    procedure ClearList();
   end;
 
 implementation
+
+procedure TSynCompletionProposalInsertList.Add(str: String);
+begin
+end;
+
+procedure TSynCompletionProposal.AddItem(str: String; str2: String);
+begin
+end;
+
+procedure TSynCompletionProposal.ClearList();
+begin
+end;
+
+procedure TCustomSynEdit.GetHighlighterAttriAtRowColEx();
+begin
+end;
+
+function TSynCompletionProposalForm.CurrentEditor() : TCustomSynEdit;
+begin
+  Result:=TCustomSynEdit.Create;
+end;
 
 end.
 
